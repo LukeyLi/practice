e649d4b8157bd3a649334f8d10f001f92228bafa
lzy <844201925@qq.com>
2020-01-28 12:40:30 +0800

    原型模式

---
+++ src/main/java/com/lzy/design/pattern/prototype/abstractprototype/A.java
@@ -0,0 +1,13 @@
+package com.lzy.design.pattern.prototype.abstractprototype;
+
+/**
+ * @description:
+ * @author: lzy
+ * @create: 2020-01-28 11:48
+ **/
+public abstract class A implements Cloneable {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
