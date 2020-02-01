20011c200100500544592ab02fcdefa3ac9c26be
lzy <844201925@qq.com>
2020-02-01 16:32:26 +0800

    rabbitmq Qos

---
+++ src/main/java/com/lzy/design/rabbitmq/t12_qos/QosProducer.java
@@ -0,0 +1,38 @@
+package com.lzy.design.rabbitmq.t12_qos;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @description:
+ * @author: lzy
+ * @create: 2020-02-01 16:16
+ **/
+@Slf4j
+public class QosProducer {
+    public static final String EXCHANGE_NAME = "qos_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+        String routingKey = "error";
+        for (int i = 0; i < 30; i++) {
+            String message = "hello rabbitmq " + i;
+            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes());
+            log.info("send message, routingKey: {}, message: {}", routingKey ,message);
+        }
+        channel.close();
+        connection.close();
+    }
+}
